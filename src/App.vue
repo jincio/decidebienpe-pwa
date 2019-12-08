5b083db7eb22394977fc73afb95ff9af73ea8838
Luis Salas <luisszkl@gmail.com>
2019-12-07 22:59:09 -0500

    Usar Vue Router para generar query params de páginas que puedan ser capturadas por Google Analytics (y deep linking) #1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <v-app id="app">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
+    <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
-        <v-list-item link :to="{path:'/'}">
+        <v-list-item link :to="{ path: '/' }">
           <v-list-item-action>
             <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            Bienvenidos
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link @click="routeFiltros">
+          <v-list-item-action>
+            <v-icon>mdi-filter</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             Filtros
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link :to="{path:'/resumen'}">
+        <v-list-item link :to="{ path: '/resumen' }">
           <v-list-item-action>
             <v-icon>mdi-file-document-box</v-icon>
           </v-list-item-action>
@@ -21,7 +26,7 @@
             Resumen General
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link :to="{path:'/creditos'}">
+        <v-list-item link :to="{ path: '/creditos' }">
           <v-list-item-action>
             <v-icon>mdi-contact-mail</v-icon>
           </v-list-item-action>
@@ -32,40 +37,40 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      color="indigo"
-      dark
-    >
+    <v-app-bar app color="indigo" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>¡Decide bien! Elecciones congresales Perú 2020</v-toolbar-title>
+      <v-toolbar-title
+        >¡Decide bien! Elecciones congresales Perú 2020</v-toolbar-title
+      >
     </v-app-bar>
 
     <v-content>
-      <v-container
-        class=""
-        fluid
-      >
-              <router-view/>
-    
+      <v-container class="" fluid>
+        <router-view />
       </v-container>
     </v-content>
-    <v-footer
-      color="indigo"
-      app
-    >
+    <v-footer color="indigo" app>
       <span class="white--text">&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-    }),
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  }),
+  methods: {
+    routeFiltros() {
+      if (this.$route.name !== "filtros") {
+        this.$router.push({
+          path: "/filtros"
+        });
+      }
+    }
   }
+};
 </script>
